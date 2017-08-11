638fa4ce1a276c6231cb1886e125f149c1154de1
Bala Dasoji <bala.dasoji@gmail.com>
2017-08-11 19:33:14 +0200

    Added Local k8s resources and updated HC.java

--- microservices/spring/hello/src/main/java/hello/HelloController.java
+++ microservices/spring/hello/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! for Mr Dasoji";
+        return "Greetings from Spring Boot! for Mr Dasoji This is built on local linux";
     }
 
 }
