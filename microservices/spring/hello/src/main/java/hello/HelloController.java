14e515e7b80ea65d126689a419df45947d41546c
Bala Dasoji <bala.dasoji@gmail.com>
2017-09-28 04:26:55 +0000

    Updated with JWT decoder

--- microservices/spring/hello/src/main/java/hello/HelloController.java
+++ microservices/spring/hello/src/main/java/hello/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 @RestController
 public class HelloController {
 
@@ -15,15 +18,35 @@ private HttpServletRequest context;
     @RequestMapping("/")
     public String index() {
 	String allheaders = "";
+	DecodedJWT jwt = null ;
+	String token="" ;
+	String sub="" ;
+	String name ="";
 	Map<String, String> map = new HashMap<String, String>();
 
         Enumeration headerNames = context.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             String key = (String) headerNames.nextElement();
             String value = context.getHeader(key);
-	    allheaders = allheaders + key + value + "\r" ;
+		if (key.equals("authorization"))
+		{
+			token=value;
+			token = token.substring(7);
+			
+		}
+	    allheaders = allheaders + key + value ;
 	}
-        return "Greetings from Spring Boot! for Mr Dasoji" +allheaders;
+	if (!token.equals(""))
+	{
+		try {
+		    jwt = JWT.decode(token);
+			sub =jwt.getSubject();
+			name = jwt.getIssuer();
+		} catch (JWTDecodeException exception){
+		    exception.printStackTrace(System.err);
+		}
+	}
+        return "Greetings from Spring Boot! for Mr Dasoji JWT is " +jwt + " subject is  " +sub + " Issuer is "+ name;
     }
 
 }
