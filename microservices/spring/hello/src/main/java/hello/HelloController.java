13767da3d92242a8fbdb10cd16cd010b0667731e
Bala Dasoji <bala.dasoji@gmail.com>
2017-09-28 03:34:06 +0000

    Updated with servlet variables

--- microservices/spring/hello/src/main/java/hello/HelloController.java
+++ microservices/spring/hello/src/main/java/hello/HelloController.java
@@ -1,14 +1,29 @@
 package hello;
 
+import java.util.*;
+import javax.servlet.http.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+@Autowired
+private HttpServletRequest context;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! for Mr Dasoji This is built on local linux";
+	String allheaders = "";
+	Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = context.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = context.getHeader(key);
+	    allheaders = allheaders + key + value + "\r" ;
+	}
+        return "Greetings from Spring Boot! for Mr Dasoji" +allheaders;
     }
 
 }
