764fc4b2073291126a6ba9db0245e6cb1f8ee020
Bala Dasoji <bala.dasoji@maersk.com>
2017-10-17 13:46:12 +0200

    Changes to static resources

--- microservices/spring/webfinger/src/main/resources/static/t.js
+++ microservices/spring/webfinger/src/main/resources/static/t.js
@@ -1,5 +1,9 @@
 var hash = false;
 var jwtb = false; 
+var id_token;
+var access_token;
+var token_type;
+var state;
 //checkHash();
 
 function checkHash(){ 
@@ -9,13 +13,27 @@ function checkHash(){
     } t=setTimeout("checkHash()",400); 
 }
 
-function processHash(hash){
-var fields = hash.split(/=/);
-var variable = fields[0];
-var jwt=fields[1];
-  //  alert(jwt);
+function GetQueryStringParams(sParam)
+{
+    var sFragment = window.location.hash;
+    var sURLVariables = sFragment.split('&');
 
-jwtb = "Bearer "+jwt;
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+function processHash(hash){
+
+id_token = GetQueryStringParams("id_token");
+access_token = GetQueryStringParams("access_token");
+token_type = GetQueryStringParams("token_type");
+state = GetQueryStringParams("state");
 
 }
 
@@ -24,17 +42,20 @@ function getVendorInvoice()
 {
 
 checkHash();
-var XMLReq = new XMLHttpRequest();
+      document.getElementById("idtoken").innerHTML = id_token;
+      document.getElementById("accesstoken").innerHTML = access_token;
+      document.getElementById("tokentype").innerHTML = token_type;
+      document.getElementById("state").innerHTML = state;
+//var XMLReq = new XMLHttpRequest();
 
-XMLReq.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
-var vid = document.getElementById("vendorid").value;
-XMLReq.open("GET", "http://localhost:8080/showInvoice?vendorid="+vid, true );
-XMLReq.setRequestHeader("Authorization", jwtb);
-XMLReq.send(null);
+//XMLReq.onreadystatechange = function() {
+//    if (this.readyState == 4 && this.status == 200) {
+//    }
+//  };
+//var vid = document.getElementById("vendorid").value;
+//XMLReq.open("GET", "http://localhost:8080/showInvoice?vendorid="+vid, true );
+//XMLReq.setRequestHeader("Authorization", jwtb);
+//XMLReq.send(null);
 }
 
 
