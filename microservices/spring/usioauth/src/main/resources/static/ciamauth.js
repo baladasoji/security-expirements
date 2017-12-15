9d9cd7fc7c86805b6f548cddf4ec51a02b1f43c1
baladasoji <bala.dasoji@gmail.com>
2017-12-15 12:18:22 +0100

    Real GEO API calls

--- microservices/spring/usioauth/src/main/resources/static/ciamauth.js
+++ microservices/spring/usioauth/src/main/resources/static/ciamauth.js
@@ -70,8 +70,8 @@ cityXMLReq.onreadystatechange = function() {
     document.getElementById("cities").className='noauthrest'
   }
   };
-	 cityXMLReq.open("GET", "https://api159165live.gw-us-east.akana.com/", true);
-//	cityXMLReq.open("GET", "https://api205063live.gateway.akana.com/maeu/locations/cities?cityprefix=az", true );
+	// Working request cityXMLReq.open("GET", "https://api159165live.gw-us-east.akana.com/", true);
+	cityXMLReq.open("GET", "https://apit.maerskline.com/maeu/locations/cities?cityprefix=hn", true );
 	cityXMLReq.setRequestHeader("Authorization",jwtb );
 	cityXMLReq.send(null);
 }
